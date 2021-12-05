0d703e828d5710278f9a10e93db186fb72b9b8d3
Cezary Piątek <cezarypiatek@live.com>
2021-12-05 15:01:33 +0100

    Add TrimStart and TrimEnd extenions

---
+++ src/TextExtensions.Tests/TrimExtensionsTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace TextExtensions.Tests
+{
+    public class TrimExtensionsTests
+    {
+        [TestCase(null, null, null)]
+        [TestCase("", null, "")]
+        [TestCase("", "", "")]
+        [TestCase("testData", "test", "Data")]
+        [TestCase("testData", "extra", "testData")]
+
+        public void should_trim_leading_text(string input, string toTrim, string expectedResult)
+        {
+            Assert.AreEqual(expectedResult, input.TrimStart(toTrim));
+        }
+        
+        
+        [TestCase(null, null, null)]
+        [TestCase("", null, "")]
+        [TestCase("", "", "")]
+        [TestCase("testData", "Data", "test")]
+        [TestCase("testData", "extra", "testData")]
+
+        public void should_trim_trailing_text(string input, string toTrim, string expectedResult)
+        {
+            Assert.AreEqual(expectedResult, input.TrimEnd(toTrim));
+        }
+    }
+}
