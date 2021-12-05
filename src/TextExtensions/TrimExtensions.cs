b1476b055b98c6db78098b323d4b0b85ff720292
Cezary Piątek <cezarypiatek@live.com>
2021-12-05 15:01:33 +0100

    Add TrimStart and TrimEnd extenions

---
+++ src/TextExtensions/TrimExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace TextExtensions;
+
+public static class TrimExtensions
+{
+    [return: NotNullIfNotNull("input")]
+    public static string? TrimStart(this string? input, string? toRemove)
+    {
+        if (input == null)
+        {
+            return null;
+        }
+
+        if (toRemove == null)
+        {
+            return input;
+        }
+
+        return input.StartsWith(toRemove) ? input.Substring(toRemove.Length) : input;
+    }
+
+    [return: NotNullIfNotNull("input")]
+    public static string? TrimEnd(this string? input, string? toRemove)
+    {
+        if (input == null)
+        {
+            return null;
+        }
+
+        if (toRemove == null)
+        {
+            return input;
+        }
+
+        return input.EndsWith(toRemove) ? input.Substring(0, input.Length - toRemove.Length) : input;
+    }
+}
